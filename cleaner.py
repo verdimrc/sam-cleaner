79fa3e1240cf9a8b4e1eed4615f8fb4c7e04fc8c
Verdi March <marcverd@amazon.com>
2018-04-30 15:54:03 +0800

    Cleaner lambda uses dynamic dispatch by event name

--- cleaner.py
+++ cleaner.py
@@ -4,53 +4,35 @@ from itertools import groupby, chain
 import json
 import logging
 import os
+import sys
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 TABLE_NAME = os.environ['TABLE_NAME']
+EVENT_TYPE = ['register', 'cleanup', 'asg_test_notification']
+EVENT_TYPE.append('other')    # The last entry must be 'other'.
 
 def lambda_handler(event, context):
     logger.debug(f'Event: {event}')
-
     event_type, message = parse_event(event)
-    if event_type == 'REGISTER':
-        return register(message) 
-    elif event_type == 'CLEANUP':
-        #return cleanup(message, ec2instanceid='test', del_test_item=False)
-        return cleanup(message)
-    elif event_type == 'ASG_TEST_NOTIFICATION':
-        logger.info('Ignored: ASG test notification')
-    else:
-        raise ValueError('Invalid event')
+    return dispatch(event_type, message)
 
-def parse_event(event):
-    OTHER = 'OTHER'
-    message = {}
-    
-    # Parse payload
-    try:
-        sns_payload = event['Records'][0]['Sns']
-        message = json.loads(sns_payload['Message'])
-        attrib = sns_payload['MessageAttributes']
-    except:
-        return OTHER, {}
+def dispatch(event_type, message):
+    assert event_type in EVENT_TYPE
+    fun = getattr(sys.modules[__name__], event_type)
+    return fun(message)
 
-    # Determine event type
-    if is_register_event(message, attrib):
-        event_type = 'REGISTER'
-    elif is_cleanup_event(message, attrib):
-        event_type = 'CLEANUP'
-    elif is_asg_test_notification(message, attrib):
-        event_type = 'ASG_TEST_NOTIFICATION'
-    else:
-        event_type = OTHER
-
-    return event_type, message
+def other(message):
+    logger.error('Invalid event')
 
 def register(message):
+    logger.info(f'Register: {message["instance"]}, {message["name"]}')
     table = boto3.resource('dynamodb').Table(TABLE_NAME)
     table.put_item(Item=message)
+
+def asg_test_notification(message):
+    logger.info('Ignored: ASG test notification')
 
 def cleanup(message, ec2instanceid: str=''):
     '''
@@ -78,6 +60,34 @@ def cleanup(message, ec2instanceid: str=''):
 
     logger.info(f'Clean-up done for instance {ec2instanceid}')
 
+# For testing; require existing DynamoDB items.
+#cleanup = lambda msg: cleanup(msg, ec2instanceid='test', del_test_item=False)
+
+def parse_event(event):
+    '''Determine event type, and get the payload.
+
+    :returns: (event_type, message) where event_type is in EVENT_TYPE
+    :rtype: (str, dict)
+    '''
+    event_type, message = EVENT_TYPE[-1], {}
+
+    # Parse payload
+    try:
+        sns_payload = event['Records'][0]['Sns']
+        message = json.loads(sns_payload['Message'])
+        attrib = sns_payload['MessageAttributes']
+    except:
+        return event_type, {}
+
+    # Determine event type: cycle all EVENT_TYPE but 'other'.
+    for evtype in EVENT_TYPE[:-1]:
+        is_evtype = getattr(sys.modules[__name__], f'is_{evtype}_event')
+        if is_evtype(message, attrib):
+            event_type = evtype
+            break
+
+    return event_type, message
+
 def falsify_exception(f):
     '''
     :returns: False on exception, else the return value of f
@@ -99,7 +109,7 @@ def is_cleanup_event(message, attrib):
             and message['Event'] == 'autoscaling:EC2_INSTANCE_TERMINATE')
 
 @falsify_exception
-def is_asg_test_notification(message, attrib):
+def is_asg_test_notification_event(message, attrib):
     return ('AutoScalingGroupARN' in message
             and message['Event'] == 'autoscaling:TEST_NOTIFICATION')
 
@@ -118,7 +128,6 @@ def del_group(table, instance, service, resources):
         except:
             from traceback import format_exc
             logger.error(f'Resource {r["name"]}:\n{format_exc()}')
-            pass
         finally:
             table.delete_item(Key=dict(instance=instance, name=r['name']))
 
